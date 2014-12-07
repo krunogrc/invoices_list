e052341d954d8c2f1c2a642b0f14241b5d3322d2
Kruno <krunogr@>
2014-12-07 20:35:07 +0100

    sql db

---
+++ assets/dump.sql
@@ -0,0 +1,79 @@
+-- MySQL dump 10.13  Distrib 5.1.66, for apple-darwin12.2.0 (i386)
+--
+-- Host: localhost    Database: shopbox
+-- ------------------------------------------------------
+-- Server version	5.1.66
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `invoice_lines`
+--
+
+DROP TABLE IF EXISTS `invoice_lines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoice_lines` (
+  `line_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `invoice_id` int(11) unsigned NOT NULL DEFAULT '0',
+  `line_description` varchar(255) NOT NULL DEFAULT '',
+  `quantity` int(11) unsigned NOT NULL DEFAULT '0',
+  `price` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`line_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoice_lines`
+--
+
+LOCK TABLES `invoice_lines` WRITE;
+/*!40000 ALTER TABLE `invoice_lines` DISABLE KEYS */;
+INSERT INTO `invoice_lines` VALUES (1,1,'Sushi at half price',2,49),(2,1,'Wellness-weekend ',1,99),(3,2,'Coffee 500 gr.',7,9),(4,2,'Milk',2,1);
+/*!40000 ALTER TABLE `invoice_lines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `invoices`
+--
+
+DROP TABLE IF EXISTS `invoices`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `invoices` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `customer_id` int(11) unsigned NOT NULL,
+  `invoice_date` int(11) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `invoices`
+--
+
+LOCK TABLES `invoices` WRITE;
+/*!40000 ALTER TABLE `invoices` DISABLE KEYS */;
+INSERT INTO `invoices` VALUES (1,1,1342702626),(2,2,1352702626);
+/*!40000 ALTER TABLE `invoices` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-11-13 11:35:53
--- include/connect_mysql.php
+++ include/connect_mysql.php
@@ -7,13 +7,13 @@
 //connect
 try {
 
-    $con = new PDO('mysql:host=eu-cdbr-cb-west-01.cleardb.net;dbname=cb_mnarudzbe_db', 'be57c910dd7384', 'fd3be959'); //our new PDO Object
+    $con = new PDO('mysql:host=localhost;dbname=dump', 'root', ''); //our new PDO Object
 } catch (PDOException $e) {
 
     echo $e->getMessage(); //catch and show the error
 }
 # creating the statement
-$STH = $con->query('SELECT line_id, invoice_id, line_description, quantity, price from invoice_invoice_lines');
+$STH = $con->query('SELECT line_id, invoice_id, line_description, quantity, price from invoice_lines');
 # setting the fetch mode
 $STH->setFetchMode(PDO::FETCH_OBJ);
 # showing the results
@@ -21,7 +21,7 @@ while ($row = $STH->fetch()) {
     $Invoice_line_all[] = new Invoice_line($row->line_id, $row->invoice_id, $row->line_description, $row->quantity, $row->price);
 }
 # creating the statement
-$STH = $con->query('SELECT id, customer_id, invoice_date from invoice_invoices');
+$STH = $con->query('SELECT id, customer_id, invoice_date from invoices');
 # setting the fetch mode
 $STH->setFetchMode(PDO::FETCH_OBJ);
 # showing the results
